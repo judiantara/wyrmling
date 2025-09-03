d724003b6a3e97f6ccecc792e967176b8f591de6
Baju Judiantara <judiantara@gmail.com>
2025-09-03 18:08:15 +0700

    My NixOS Machines

--- machines/toothless/default.nix
+++ machines/toothless/default.nix
@@ -1,6 +1,22 @@
 {hostname, inputs, outputs, lib, config, pkgs, ...}:
 {
   imports = [
+    ../../hardware/amd.nix
+    ../../hardware/keyboard.nix
+    ../../config/system.nix
+    ../../config/default-packages.nix
+    ../../config/default-user.nix
+    ../../config/latest-kernel.nix
+    ../../config/plasma-desktop.nix
+    ../../config/networkmanager.nix
+    ../../config/ssh.nix
+    ../../config/ssl.nix
+    ../../config/dns.nix
+    ../../config/pipewire-audio.nix
+    ../../config/age-encryption.nix
+    ../../config/kdeconnect.nix
+    ../../config/git.nix
+    ../../users/${user}.nix
     ./disk.nix
     ./luks.nix
   ];
